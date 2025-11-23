90c31b8a9e2db524ee46c0e69a4d194ab2a4d30e
CMBorjas <118933346+CMBorjas@users.noreply.github.com>
2025-11-22 21:19:04 -0700

    Footer created to better control the navigation.
    Layout changed for images, font color and size and added new imports
    
    Page.module.ccs added for change control
    
    Pages changed for sytles import.
    
    Global rules added for coherent styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Link from 'next/link'
+import styles from './page.module.css'
 
 export default function Home() {
   return (
     <section>
   <div className="bg-slate-800/95 rounded-lg p-4 shadow-sm">
-        <h2 className="text-3xl font-bold text-brand-pink">About Me</h2>
+        <h2 className={`text-3xl font-bold ${styles.aboutTitle}`}>ABOUT ME</h2>
         <p className="mt-4">I am Christian Mandujano Borjas, an aspiring software engineer currently pursuing a degree in Computer Science at the University of Colorado Denver. With a strong foundation in programming languages like Python, Java, and C++, I have a passion for solving complex problems and building efficient systems.</p>
         <p className="mt-3">I am currently working on a full stack web development project using HTML, CSS, and JavaScript. I have experience with database management using MySQL and SQLite, and I am familiar with cloud services like Google firebase and Amazon Web Services. I am also interested in cybersecurity and have experience with virus protection, firewall management, and backup & recovery.</p>
         <p className="mt-3">My experience as an I.T. Technician at The Monarch Casino and as a CIS Lab Assistant at the Community College of Denver has given me hands-on experience in hardware maintenance, networking, and team collaboration. I have worked with technologies such as Docker, Git, and cloud services like Google Cloud.</p>
